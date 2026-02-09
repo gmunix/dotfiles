e9c76c8eb6e6ed1df81c997c1909d2d89802b517
Gustavo Muniz <gusmunuizsch@gmail.com>
2026-02-09 12:10:05 -0300

    chore(nvim): remove redundant stylua lsp override

--- nvim/.config/nvim/lua/lsp/stylua.lua
+++
@@ -1,7 +0,0 @@
--- Override the stylua server definition to prevent it from autostarting as an LSP.
--- We use the CLI formatter via conform.nvim instead of running a stylua language server.
----@type vim.lsp.Config
-return {
-	autostart = false,
-	filetypes = {},
-}
