3da8e1ff42fa7a95444c66d34e8675fdd109cb29
Gustavo Muniz <gusmunizsch@gmail.com>
2026-03-07 16:11:43 -0300

    fix

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,24 +1,26 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-  },
-  config = function()
-    vim.notify = require("notify")
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		vim.notify = require("notify").setup({
+			background_colour = "#000000",
+		})
 
-    require("noice").setup({
-      lsp = {
-        hover = { silent = false },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = false,
-        lsp_doc_border = false,
-      },
-    })
-  end,
+		require("noice").setup({
+			lsp = {
+				hover = { silent = false },
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+				inc_rename = false,
+				lsp_doc_border = false,
+			},
+		})
+	end,
 }
