71d4d161a4802d9cff19080ee209e85af39a8178
Gustavo Muniz <gusmunizsch@gmail.com>
2026-03-24 19:46:21 -0300

    fix(zsh): reorganize and add lost aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,12 +1,20 @@
-alias vi="nvim"
 alias ll="ls -la"
+
 alias tn="tmux new -s"
 alias ta="tmux a"
 alias ts="tmux ls"
+
 alias td="tmux-dev"
-alias lg="lazygit"
 alias wl="worktree-list"
 alias wn="worktree-new"
 alias wo="worktree-open"
 alias wd="worktree-delete"
 alias dotf="tmux-dev dotfiles $HOME/dotfiles"
+
+alias gl="git log"
+alias gs="git status"
+alias lg="lazygit"
+
+alias op="opencode --port"
+alias oc="opencode"
+alias vi="nvim"
