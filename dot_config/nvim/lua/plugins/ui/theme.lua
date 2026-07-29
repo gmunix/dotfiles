d152cbb77a5e28b7cb3ed8f5bb98ce73ff3c4adc
Gustavo Muniz <gusmunizsch@gmail.com>
2026-07-28 23:09:16 -0300

    feat: add theme with dark and light modes to opencode neovim and ghostty

--- dot_config/nvim/lua/plugins/ui/theme.lua
+++ dot_config/nvim/lua/plugins/ui/theme.lua
@@ -1,24 +1,12 @@
 return {
-	"gthelding/monokai-pro.nvim",
-	config = function()
-		require("monokai-pro").setup({
-			transparent_background = true,
-			filter = "ristretto",
-			override = function()
-				return {
-					NonText = { fg = "#948a8b" },
-					MiniIconsGrey = { fg = "#948a8b" },
-					MiniIconsRed = { fg = "#fd6883" },
-					MiniIconsBlue = { fg = "#85dacc" },
-					MiniIconsGreen = { fg = "#adda78" },
-					MiniIconsYellow = { fg = "#f9cc6c" },
-					MiniIconsOrange = { fg = "#f38d70" },
-					MiniIconsPurple = { fg = "#a8a9eb" },
-					MiniIconsAzure = { fg = "#a8a9eb" },
-					MiniIconsCyan = { fg = "#85dacc" },
-				}
-			end,
-		})
-		vim.cmd([[colorscheme monokai-pro]])
+	"polirritmico/monokai-nightasty.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {
+		dark_style_background = "transparent",
+		light_style_background = "transparent",
+	},
+	config = function(_, opts)
+		require("monokai-nightasty").load(opts)
 	end,
 }
